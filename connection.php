d4d08f7c7ef73bd75b3e198c071d53d0ae48407f
Nicky Hendrik Sen <49971362+NickyHendrikSen@users.noreply.github.com>
2020-01-07 12:05:36 +0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+    
+    $connection = new mysqli("localhost", "nicz3614_4163", "hm=RQ_^w_-1C", "nicz3614_itemQ");
+    if(!$connection){
+        echo "Connection Error";
+        exit();
+    }
+?>
